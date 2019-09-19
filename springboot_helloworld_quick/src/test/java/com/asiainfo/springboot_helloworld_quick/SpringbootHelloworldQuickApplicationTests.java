5edccc89f1fafd0423d1496fee6fd908b5ba86bd
yangshuang <yangshuang@asiainfo.com>
2019-09-19 23:23:15 +0800

    yml

--- springboot_helloworld_quick/src/test/java/com/asiainfo/springboot_helloworld_quick/SpringbootHelloworldQuickApplicationTests.java
+++ springboot_helloworld_quick/src/test/java/com/asiainfo/springboot_helloworld_quick/SpringbootHelloworldQuickApplicationTests.java
@@ -1,7 +1,9 @@
 package com.asiainfo.springboot_helloworld_quick;
 
+import com.asiainfo.springboot_helloworld_quick.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootHelloworldQuickApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
