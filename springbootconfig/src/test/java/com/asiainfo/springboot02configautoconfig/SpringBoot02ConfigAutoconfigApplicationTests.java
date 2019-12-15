84a1f3ec9f314b691f74c346a28b3acc69d4f0d3
yangshuang <yangshuang@asiainfo.com>
2019-12-15 17:21:01 +0800

    a

---
+++ springbootconfig/src/test/java/com/asiainfo/springboot02configautoconfig/SpringBoot02ConfigAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiainfo.springboot02configautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02ConfigAutoconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
